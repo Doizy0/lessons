0479719817925d8b65e213995996504803816561
DOizy <hey.hey111@mail.ru>
2023-02-25 18:24:50 +0300

    task3 updated

--- Lesson#1/Task3.py
+++ Lesson#1/Task3.py
@@ -3,37 +3,31 @@ class MatrixGame:
         self.n = n
         self.m = m
         self.matrix = []
-        for i in range(self.n):
-            self.matrix.append([0 for _ in range(self.m)])
-
-
 
     def read_matrix(self):
         for i in range(self.n):
-            self.matrix[i] = [int(num) for num in input().split()]
+            self.matrix.append([int(num) for num in input().split()])
 
     def lower_value(self):
         d = 0
         lower_values = []
         for i in range(self.n):
-
             lower_values.append(min(self.matrix[i]))
         return max(lower_values)
+
     def upper_value(self):
         k = 0
         upper_values = []
-        for i in range(self.n):
-            for j in range(self.m):
-                k = self.matrix[i][j]
-                upper_values.append(max(k))
+        for j in range(self.m):
+            column = []
+            for i in range(self.n):
+                column.append(self.matrix[i][j])
+            upper_values.append(max(column))
         return min(upper_values)
-
-
-
 
 
 n, m = map(int, input().split())
 
-
 game = MatrixGame(n, m)
+game.read_matrix()
 print(game.lower_value(), game.upper_value())
