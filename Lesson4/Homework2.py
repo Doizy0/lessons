ab4dd14e126532d6ada4457b7e8ffb7e3c6503ce
DOizy <hey.hey111@mail.ru>
2023-03-08 19:53:59 +0300

    ne

--- Lesson4/Homework2.py
+++ Lesson4/Homework2.py
@@ -1,18 +1,33 @@
 #Произведение матриц
 #Заданы две целочисленные матрицы A и B. Матрица A состоит из N строк и M столбцов, Матрица B состоит из M строк и P столбцов. Требуется вычислить произведение данных матриц A*B.
+
+
+
+
 n, m, p = map(int, input().split())
+
+
+a = []
 for i in range(n):
-    a = list(map(int, input().split()))
-input()
+    row1 = list(map(int, input().split()))
+    for j in range(m):
+        a[i][j] = row1[j]
+
+
+b = []
 for i in range(m):
-    b = list(map(int, input().split()))
-
-
-c = [[0 for j in range(p)] for i in range(n)]
-for i in range(n):
+    row2 = list(map(int, input().split()))
     for j in range(p):
+        b[i][j] =row2[j]
+
+c = []
+for i in range(n):
+    row3 = []
+    for j in range(p):
+
+        sum = 0
         for k in range(m):
-            c[i][j] += a[i][k] * b[k][j]
+            sum += a[i][k] * b[k][j]
+        row3.append(sum)
+    c.append(row3)
 
-
-print(c)
