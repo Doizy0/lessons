a21b85db0c590b2e5fe06aba1ed5a96dce39b082
DOizy <hey.hey111@mail.ru>
2023-03-16 18:57:32 +0300

    new

---
+++ Lesson5/Homework1.py
@@ -0,0 +1,19 @@
+#Требуется выполнить сортировку временных моментов, заданных в часах, минутах и секундах.
+#https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=7&id_topic=118&id_problem=728
+n = int(input())
+times = []
+for i in range(n):
+    h, m, s = map(int, input().split())
+    times.append((h, m, s))
+
+for i in range(n):
+    key = times[i]
+    j = i - 1
+    while j >= 0 and times[j] > key:
+        times[j + 1] = times[j]
+        j -= 1
+    times[j + 1] = key
+
+print(*times, end=' ')
+print()
+
