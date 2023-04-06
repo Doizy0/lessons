8ee3159fe4333591406d10eb19a55149197fb289
DOizy <hey.hey111@mail.ru>
2023-04-06 19:16:00 +0300

    new

--- Lesson5/homework7.py
+++ Lesson5/homework7.py
@@ -1,44 +1,45 @@
+from datetime import datetime
+from calendar import monthrange
+
+
+def dt_diff(start_dt: datetime, end_dt: datetime):
+    total_minutes = (end_dt - start_dt).total_seconds() / 60
+    number_of_days = 0
+    if end_dt.month != start_dt.month:
+        number_of_days = (monthrange(2023, start_dt.month)[1] - start_dt.day) + end_dt.day
+    elif end_dt.day != start_dt.day:
+        number_of_days = end_dt.day - start_dt.day
+
+    to_subtract = number_of_days * 960
+
+    return total_minutes - to_subtract + 1
+
+
+days = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31}
+months = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 
 k = int(input())
 leaflets = []
 for i in range(k):
     line = input()
-    if not line:
-        continue
     day_month_parts, time_parts = line.split()
-    if not day_month_parts:
-        continue
-    day, month = map(int, day_month_parts.split('.'))
+    day, month = map(int, day_month_parts.split('.')[:-1])
     hour, minute = map(int, time_parts.split(':'))
-    leaflets.append((day, month, hour, minute))
+    #if month in months and day in days:
+        #continue
+    leaflets.append(datetime(2023, month, day, hour, minute))
 
 leaflets.sort()
 
-total_work_time = 0
-current_case_start_time = None
-for day, month, hour, minute, leaflet_type in leaflets:
-    dt = (day, month, hour, minute)
-    if leaflet_type == 0:
+total_minutes = 0
 
-        current_case_start_time = dt
-    else:
+for i in range(1, len(leaflets), 2):
+    end = leaflets[i]
+    start = leaflets[i - 1]
+    total_minutes += dt_diff(start, end)
 
-        case_duration = ((day - current_case_start_time[0]) * 1440 +
-                         (month - current_case_start_time[1]) * 43200 +
-                         (hour - current_case_start_time[2]) * 60 +
-                         (minute - current_case_start_time[3]))
-        if current_case_start_time[2] < 10:
-            current_case_start_time = (current_case_start_time[0], current_case_start_time[1], 10, 0)
-        if hour > 18 or (hour == 18 and minute > 0):
-            dt = (day, month, 18, 0)
-        if current_case_start_time[0] % 4 == 0:
-
-            work_time = case_duration / 527040
-        else:
-
-            work_time = case_duration / 525600
-        total_work_time += work_time
-
-hours = int(total_work_time * 24)
-minutes = int((total_work_time * 24 * 60) % 60)
-print(hours+':'+minutes)
+hours = int(total_minutes // 60)
+minutes = str(int(total_minutes % 60))
+displayed_minutes = '0' + minutes if len(minutes) == 1 else minutes
+print(hours, ':', displayed_minutes, sep='')
